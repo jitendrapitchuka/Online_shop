72f27c0595438460b611eb7b434577ad43484875
jithendra pitchuka <jithendra2900@gmail.com>
2021-12-16 16:21:10 +0530

    added products

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 class Category(models.Model):
     name = models.CharField(max_length=200,db_index=True)
     slug = models.SlugField(max_length=200,unique=True)
@@ -11,6 +11,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
@@ -29,3 +32,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',args=[self.id, self.slug])
